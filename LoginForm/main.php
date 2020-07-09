9f707afc9c32c6bb8510f27207920d4ef009aa63
SzymonLeja <szymon.leja3@gmail.com>
2020-07-09 17:15:44 +0200

    Update main.php

--- LoginForm/main.php
+++ LoginForm/main.php
@@ -9,7 +9,7 @@ require_once './includes/session.php';
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>SkyBlueEducation</title>
-    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="./includes/styles.css" />
   </head>
   <body>
     <div class="wrapper">
