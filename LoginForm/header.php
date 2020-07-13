6ac29407a049c0db2dc59be267e8df0109d00254
SzymonLeja <szymon.leja3@gmail.com>
2020-07-13 15:05:31 +0200

    Sorting files

--- LoginForm/header.php
+++ LoginForm/header.php
@@ -8,7 +8,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SkyBlueEducation</title>
-    <link rel="stylesheet" href="./includes/login.css">
+    <link rel="stylesheet" href="./includes/css/login.css">
 </head>
 
 <body>
